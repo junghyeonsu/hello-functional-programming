70df58540723180db7790b0ec593110ccedc0628
junghyeonsu <jung660317@naver.com>
2023-01-23 22:27:09 +0900

    Update _.js

--- _.js
+++ _.js
@@ -1,17 +1,72 @@
 function _filter(list, predicate) {
 	var new_list = [];
-	for (var i = 0; i < list.length; i++) {
-		if (predicate(list[i])) {
-			new_list.push(list[i]);
+
+	_each(list, function(val) {
+		if (predicate(val)) {
+			new_list.push(val);
 		}
-	}
+	})
+
 	return new_list;
 }
 
 function _map(list, mapper) {
 	var new_list = [];
-	for (var i = 0; i < list.length; i++) {
-		new_list.push(mapper(list[i]));
-	}
+
+	_each(list, function(val) {
+		new_list.push(mapper(val));
+	})
+	
 	return new_list;
+}
+
+function _each(list, iter) {
+	for (var i = 0; i < list.length; i++) {
+		iter(list[i]);
+	}
+
+	return list;
+}
+
+function _curry(fn) {
+	return function(a) {
+		return arguments.length === 2 ? fn(a, b) : function(b) { return fn(a, b);}
+	}
+}
+
+function _curryr (fn) {
+	return function(a, b) {
+		return arguments.length === 2 ? fn(a, b) : function(b) { return fn(b, a);}
+	}
+}
+
+function _get(obj, key) {
+	return obj === null ? undefined : obj[key];
+}
+
+function _reduce(list, iter, memo) {
+	_each(list, function(val) {
+		memo = iter(memo, val);
+	});
+	return memo;
+}
+
+function _rest(list, num) {
+	return Array.prototype.slice.call(list, num || 1);
+}
+
+function _pipe() {
+	var fns = arguments;
+	return function(arg) {
+		return _reduce(fns, function(arg, fn) {
+			return fn(arg);
+		}, arg);
+	}
+}
+
+function _go() {
+	var fns = _rest(arguments);
+	return function(arg) {
+		return _pipe.apply(null, fns)(arg);
+	}
 }
